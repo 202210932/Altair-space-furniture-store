e56ac5e45b74e1fc6df9e3f3af86bbbb0580c3a9
Canlas <you@example.com202210932@gordoncollege.edu.ph>
2023-05-25 23:55:42 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 updateCartTotal();
+
 document.getElementById("emptycart").addEventListener("click", emptyCart);
 var btns = document.getElementsByClassName('addtocart');
 for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', function() {
-        addToCart(this);
-    });
+    btns[i].addEventListener('click', function() {addToCart(this);});
 }
+
+
 function addToCart(elem) {
+
     var sibs = [];
     var getprice;
     var getproductName;
     var cart = [];
-    var stringCart;
-    while (elem = elem.previousSibling) {
-        if (elem.nodeType === 3)
-            continue;
-        if (elem.className == "price") {
+     var stringCart;
+    while(elem = elem.previousSibling) {
+        if (elem.nodeType === 3) continue; 
+        if(elem.className == "price"){
             getprice = elem.innerText;
         }
         if (elem.className == "productname") {
@@ -24,18 +25,20 @@ function addToCart(elem) {
         sibs.push(elem);
     }
     var product = {
-        productname: getproductName,
-        price: getprice
+        productname : getproductName,
+        price : getprice
     };
     var stringProduct = JSON.stringify(product);
-    if (!sessionStorage.getItem('cart')) {
+    
+    if(!sessionStorage.getItem('cart')){
         cart.push(stringProduct);
         stringCart = JSON.stringify(cart);
         sessionStorage.setItem('cart', stringCart);
         addedToCart(getproductName);
         updateCartTotal();
-    } else {
-        cart = JSON.parse(sessionStorage.getItem('cart'));
+    }
+    else {
+       cart = JSON.parse(sessionStorage.getItem('cart'));
         cart.push(stringProduct);
         stringCart = JSON.stringify(cart);
         sessionStorage.setItem('cart', stringCart);
@@ -43,43 +46,44 @@ function addToCart(elem) {
         updateCartTotal();
     }
 }
-function updateCartTotal() {
+function updateCartTotal(){
     var total = 0;
     var price = 0;
     var items = 0;
     var productname = "";
     var carttable = "";
-    if (sessionStorage.getItem('cart')) {
+    if(sessionStorage.getItem('cart')) {
         var cart = JSON.parse(sessionStorage.getItem('cart'));
         items = cart.length;
-        for (var i = 0; i < items; i++) {
+        for (var i = 0; i < items; i++){
             var x = JSON.parse(cart[i]);
             price = parseFloat(x.price.split('$')[1]);
             productname = x.productname;
             carttable += "<tr><td>" + productname + "</td><td>$" + price.toFixed(2) + "</td></tr>";
             total += price;
         }
+        
     }
     document.getElementById("total").innerHTML = total.toFixed(2);
     document.getElementById("carttable").innerHTML = carttable;
     document.getElementById("itemsquantity").innerHTML = items;
 }
 function addedToCart(pname) {
-    var message = pname + " was added to the cart";
-    var alerts = document.getElementById("alerts");
-    alerts.innerHTML = message;
-    if (!alerts.classList.contains("message")) {
-        alerts.classList.add("message");
-    }
+  var message = pname + " was added to the cart";
+  var alerts = document.getElementById("alerts");
+  alerts.innerHTML = message;
+  if(!alerts.classList.contains("message")){
+     alerts.classList.add("message");
+  }
 }
 function emptyCart() {
-    if (sessionStorage.getItem('cart')) {
+    if(sessionStorage.getItem('cart')){
         sessionStorage.removeItem('cart');
         updateCartTotal();
-        var alerts = document.getElementById("alerts");
-        alerts.innerHTML = "";
-        if (alerts.classList.contains("message")) {
-            alerts.classList.remove("message");
-        }
+      var alerts = document.getElementById("alerts");
+      alerts.innerHTML = "";
+      if(alerts.classList.contains("message")){
+          alerts.classList.remove("message");
+      }
     }
 }
